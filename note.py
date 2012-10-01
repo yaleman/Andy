6eac8ed69badec7b1249ae5e01d25714a8da0d12
yaleman <yaleman@ricetek.net>
2012-10-02 08:44:07 +1000

    Initial Commit
    Note class is fairly solid at this point.

---
+++ note.py
@@ -0,0 +1,74 @@
+import hashlib, pickle, os
+
+class Note():
+	def __init__( self, filename, preload = True ):
+		# relies on hashlib, pickle, os
+		self.filename = filename
+		self.notes = {}
+		self.load( preload )
+		self.changed = False
+
+	def __del__( self ):
+		print "Saving notes on destruction"
+		pickle.dump( self.notes, open( self.filename, "wb" ) )
+
+	def handle_text( self, text ):
+		text = " ".join( text.split()[1:] )
+		if text.lower().startswith( "add" ):
+			self.add( " ".join( text.split()[1:] ) )
+		elif text.lower().startswith( "search" ):
+			foundany = False
+			terms = text.split()[1:]
+			print "Searching for: {}".format( ", ".join( terms ) )
+			for note in self.notes:
+				found = False
+				notelow = self.notes[note]
+				for term in terms:
+					if( term.lower() in notelow ):
+						found = True
+						if not foundany:
+							print "##################"
+						foundany = True
+						break
+				if found:	
+					print self.notes[note]
+					print "##################"
+			if not foundany:
+				print "Sorry, nothing found in notes."
+		else:
+			print "Unsure what you meant by '{}'".format( text )		
+
+	def add( self, text ):
+		# generate hash of note
+		h = hashlib.new( 'ripemd160' )
+		h.update( text )
+		h = h.hexdigest()
+		# check if hash not in notes storage
+		if h not in self.notes:
+			self.notes[h] = text
+			self.changed = True
+			return True
+		return False
+
+	def load( self, learn = True ):
+		# loads notes from file into a variable called notes
+		# if learn == True, stores the notes into self.notes
+		# if the file doesn't exist, create one
+		if( os.path.exists( self.filename ) ):
+			notes = pickle.load( open( self.filename, "rb" ) )
+			if( learn ):
+				self.notes = notes
+				self.changed = False
+			return notes
+		else:
+			self.save( True )
+
+	def save( self, force = False ):
+		# saves the file
+		# checks to see if the file's changed to avoid extra writes
+		# can be forced
+		if( self.changed or force == True ):
+			pickle.dump( self.notes, open( self.filename, "wb" ) )
+			if not force:
+				self.changed = False
+
