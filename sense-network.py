ebfa54d2f6a0501a93f4c2bc8474baee7a1c0260
yaleman <yaleman@ricetek.net>
2012-10-10 10:38:21 +1000

    moved sensenetwork to its own class for plugin possibilities

--- sense-network.py
+++ sense-network.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-# this should be run every x time to check up on what the computer's doing and report back to the database
-# the basic idea is to get a map of process -> ports/ip's used. things like web browsers will be noisy, but I'm looking
-# for those strange apps/ports that you don't know about until you're looking on wireshark.
-
 try:
 	import pexpect
 except ImportError:
@@ -12,44 +8,70 @@ import re
 import toolbox
 import config
 
-command = "lsof -i4 -L -n -P"
 
 import json
 
 #TODO: deal with ipv6 ('launchd', '1', 'root', '32u', 'IPv6', '0x78aab92d3c18db51', '0t0', 'TCP', '[::1]:631 (LISTEN)')
 
-lineparser = re.compile( "(?P<command>[a-zA-Z0-9]+)[\s]+(?P<pid>[0-9]+)[\s]+(?P<username>[\S]+)[\s]+([a-zA-Z0-9]+)[\s]+(?P<sixorfour>[A-Za-z0-9]+)[\s]+(?P<device>[a-zA-Z0-9]+)[\s]+(?P<size>[a-z0-9A-Z]+)[\s]+(?P<transport>[A-Za-z0-9]+)[\s]+(?P<details>.*)" )
+class SenseNetwork():
+	"""  this should be run every x time to check up on what the computer's doing and report back to the database
+		 the basic idea is to get a map of process -> ports/ip's used. things like web browsers will be noisy, but I'm looking
+		 for those strange apps/ports that you don't know about until you're looking on wireshark.
+		"""
+
+	def __init__( self, config ):
+		self.lineparser = re.compile( "(?P<command>[a-zA-Z0-9]+)[\s]+(?P<pid>[0-9]+)[\s]+(?P<username>[\S]+)[\s]+([a-zA-Z0-9]+)[\s]+(?P<sixorfour>[A-Za-z0-9]+)[\s]+(?P<device>[a-zA-Z0-9]+)[\s]+(?P<size>[a-z0-9A-Z]+)[\s]+(?P<transport>[A-Za-z0-9]+)[\s]+(?P<details>.*)" )
+		self.command = "lsof -i4 -L -n -P"
+		self.config = config
+		self.info = []
+		self.errors = []
+
+	def getinfo( self ):
+		if self.info:
+			return self.info
+		else:
+			return self.parse_lsof_information()[1]
+	def geterrors( self ):
+		if self.errors:
+			return self.errors
+		else:
+			return self.parse_lsof_information()[0]
 
 
-def parse_lsof_information( lines ):
-	errors = []
-	info = []
-	for line in lines:
-		if line != "" and not line.startswith( "COMMAND " ):
-			parsed = lineparser.findall( line )[0]
-			if len( parsed ) == 0:
-				errors.append( "FAULT: {}".format( line ) )
-			else:
-				details = parsed[-1]
-				src, dest, status = ( "", "", "" )
-				if( "->" in details ):
-					src, dest = details.split( "->" )
-					if( "(" in dest ):
-						dest, status = dest.split()
-				elif( "(" in details ):
-					details = details.replace( ")", "" ).replace( "(", "" )
-					srcdest, status = details.split()
-					src, dest = srcdest.split( ":" )
+	def parse_lsof_information( self ):
+		errors, lines = toolbox.sudorun( self.command, self.config.password )
+		errors = []
+		info = []
+		for line in lines:
+			if line != "" and not line.startswith( "COMMAND " ):
+				parsed = self.lineparser.findall( line )[0]
+				if len( parsed ) == 0:
+					errors.append( "FAULT: {}".format( line ) )
 				else:
-					src, dest = details.split( ":" )
-				src = src.split( ":" )
-				dest = dest.split( ":" )
-				status = status.replace( ")", "" ).replace( "(", "" )
-				info.append( [ det for det in parsed[:-1] ] + [src] + [dest] + [status ] )
-	return errors, info
+					details = parsed[-1]
+					src, dest, status = ( "", "", "" )
+					if( "->" in details ):
+						src, dest = details.split( "->" )
+						if( "(" in dest ):
+							dest, status = dest.split()
+					elif( "(" in details ):
+						details = details.replace( ")", "" ).replace( "(", "" )
+						srcdest, status = details.split()
+						src, dest = srcdest.split( ":" )
+					else:
+						src, dest = details.split( ":" )
+					src = src.split( ":" )
+					dest = dest.split( ":" )
+					status = status.replace( ")", "" ).replace( "(", "" )
+					info.append( [ det for det in parsed[:-1] ] + [src] + [dest] + [status ] )
+		self.info = info
+		self.errors = errors
+		return errors, info
 
-errors, lines = toolbox.sudorun( command, config.password )
-errors, info = parse_lsof_information( lines )
+senseNetwork = SenseNetwork( config )
+#errors, info = senseNetwork.parse_lsof_information()
+errors = senseNetwork.geterrors()
+info = senseNetwork.getinfo()
 
 dets = {}
 for line in info:
