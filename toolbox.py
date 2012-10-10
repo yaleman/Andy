23ff17221587082e90eaccc9431ae90b27f96ba3
yaleman <yaleman@ricetek.net>
2012-10-11 01:29:39 +1000

    cleaned up toolbox added run removed runprocess
    move from subprocess to pexpect

--- toolbox.py
+++ toolbox.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 try:
-	import pexpect
+		import pexpect
 except ImportError:
-	print "Failed to import pexpect"
+		print "Failed to import pexpect"
 
 import subprocess, re
 
@@ -21,24 +21,46 @@ def sudorun( command, password ):
 		lines.append( child.readline().strip() )
 	return lines
 
-def runProcess(exe):    
-    # relies on subprocess
-    p = subprocess.Popen(exe, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    while(True):
-      retcode = p.poll() #returns None while subprocess is running
-      line = p.stdout.readline()
-      yield line
-      if(retcode is not None):
-        break
+def sudorun( command, password ):
+		# uses pexpect to run a command with sudo on the command line with a given password and return the results. 
+		# relies on pexpect
+		child = pexpect.spawn ( "sudo {}".format( command ) )
+		i = child.expect( [ 'Password:*', '' ] )
+		lines = []
+		if i == 0:
+				child.sendline( password )
+		else:
+				lines.append( child.before.strip() )
+		while not child.eof() :
+				lines.append( child.readline().strip() )
+		return lines
 
+def run( command ):
+	child = pexpect.spawn ( "sudo {}".format( command ) )
+	lines = []
+	while not child.eof() :
+		lines.append( child.readline().strip() )
+	return lines
+
+
+"""def runProcess(exe):		
+		# relies on subprocess
+		p = subprocess.Popen(exe, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		while(True):
+				retcode = p.poll() #returns None while subprocess is running
+				line = p.stdout.readline()
+				yield line
+				if(retcode is not None):
+						break
+"""
 def self_ipaddr():
-	# finds a list of the local ip addresses
-	# relies on subprocess
-	# only works on POSIX systems	
-	ifconfig = subprocess.check_output( "ifconfig" )
-	re_ipfind = re.compile( "inet[6\s]{1,2}([a-f0-9\.\:]{3,})" )
-	ipaddr = []
-	for line in ifconfig.split( "\n" ):
-		if "inet" in line and re_ipfind.search( line ) != None:
-			ipaddr.append( re_ipfind.findall( line )[0] )
-	return ipaddr
+		# finds a list of the local ip addresses
+		# relies on subprocess
+		# only works on POSIX systems		
+		ifconfig = subprocess.check_output( "ifconfig" )
+		re_ipfind = re.compile( "inet[6\s]{1,2}([a-f0-9\.\:]{3,})" )
+		ipaddr = []
+		for line in ifconfig.split( "\n" ):
+				if "inet" in line and re_ipfind.search( line ) != None:
+						ipaddr.append( re_ipfind.findall( line )[0] )
+		return ipaddr
