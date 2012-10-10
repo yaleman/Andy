d537ff8321d207876c13599afea3fc1cf67ba3d4
yaleman <yaleman@ricetek.net>
2012-10-10 12:12:47 +1000

    plugins are great, added more functions to andy

--- andy.py
+++ andy.py
@@ -6,8 +6,9 @@ import os, hashlib
 # my modules
 #from note import Note
 #from are import Are
-import note, are
+import note, are, sense_network
 import toolbox
+import config
 
 
 # TODO make it so modules imported can register with the Andy object so plugins can add functionality over time
@@ -17,34 +18,54 @@ import toolbox
 
 class Andy():
 	def __init__( self ):
-		self.plugins = []
+		self.plugins = {}
 	def interact( self ):
 		text = ""
 		while( text != "quit" ):
 			text = raw_input( "# " ).strip()
 			#print "'%s'" % text
-			if text.lower().startswith( "@note" ):
-				note.handle_text( text )
+			text_lower = text.lower()
+			if text_lower.startswith( "#" ):
+				oper = text_lower.split()[0][1:]
+				if( oper == "plugins" ):
+					print ", ".join( self.plugins.keys() )
+			elif text_lower.startswith( "@" ):
+				oper = text_lower.split()[0][1:]
+				if( oper in self.plugins ):
+					if( 'handle_text' in dir( self.plugins[oper] ) ):
+						print self.plugins[oper].handle_text( text )
+					else:
+						print "{} module doesn't have handle_text".format( oper )
+				else:
+					print "Can't find {} module".format( oper )
 			else:
 				are.handle_text( text )
 		# this goes through all the registered plugins and saves them
 		for plugin in self.plugins:
-			if( 'save' in dir( plugin ) ):
-				plugin.save()
+			if( 'save' in dir( self.plugins[plugin] ) ):
+				self.plugins[plugin].save()
 	
 	def register_plugin( self, plugin ):
+		# registers plugins
+		# plugins need a self.pluginname which are unique so that when you call @plugin xxx it'll know what to ask for
+		# doesn't help to have a handle_text too
 		if plugin not in self.plugins:
-			self.plugins.append( plugin )
+			self.plugins[plugin.pluginname] = plugin
 
 
 
-andy = Andy()
-are = are.Are( "are.pickle" )
-note = note.Note( "notes.pickle" )
-andy.register_plugin( are )
-andy.register_plugin( note )
+if( __name__ == '__main__' ):
 
-are.replace( "ipaddr", toolbox.self_ipaddr() )
-are.save()
+	andy = Andy()
 
-andy.interact()
+	are = are.Are( "are.pickle" )
+	note = note.Note( "notes.pickle" )
+	andy.register_plugin( sense_network.SenseNetwork( config ) )
+	andy.register_plugin( are )
+	andy.register_plugin( note )
+
+	are.replace( "ipaddr", toolbox.self_ipaddr() )
+	are.save()
+
+
+	andy.interact()
