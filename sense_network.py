a5fd1c5dc83b46f9d02eaec5ab4be318167381fb
yaleman <yaleman@ricetek.net>
2012-10-12 11:38:55 +1000

    pylint suggestions on sense_network

--- sense_network.py
+++ sense_network.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-try:
-	import pexpect
-except ImportError:
-	print "Couldn't load pexpect"
+#try:
+#	import pexpect
+#except ImportError:
+#	print "Couldn't load pexpect"
 import re
 import toolbox
 import config
 
 
-import json
+#import json
 
 #TODO: deal with ipv6 ('launchd', '1', 'root', '32u', 'IPv6', '0x78aab92d3c18db51', '0t0', 'TCP', '[::1]:631 (LISTEN)')
 
@@ -19,23 +19,23 @@ class SenseNetwork():
 		 for those strange apps/ports that you don't know about until you're looking on wireshark.
 		"""
 
-	def __init__( self, config=config ):
+	def __init__( self, config = config ):
 		self._lineparser = re.compile( "(?P<command>[a-zA-Z0-9]+)[\s]+(?P<pid>[0-9]+)[\s]+(?P<username>[\S]+)[\s]+([a-zA-Z0-9]+)[\s]+(?P<sixorfour>[A-Za-z0-9]+)[\s]+(?P<device>[a-zA-Z0-9]+)[\s]+(?P<size>[a-z0-9A-Z]+)[\s]+(?P<transport>[A-Za-z0-9]+)[\s]+(?P<details>.*)" )
 		self._command = "lsof -i4 -L -n -P"
 		self._config = config
-		self._info = []
-		self._errors = []
+		self._lsof_info = []
+		self._lsof_errors = []
 		self.pluginname = "sense-network"
 		
 
 	def getinfo( self ):
-		if self._info:
-			return self._info
+		if self._lsof_info:
+			return self._lsof_info
 		else:
 			return self._parse_lsof_information()[1]
 	def geterrors( self ):
-		if self._errors:
-			return self._errors
+		if self._lsof_errors:
+			return self._lsof_errors
 		else:
 			return self._parse_lsof_information()[0]
 
@@ -55,11 +55,11 @@ class SenseNetwork():
 		lines = toolbox.sudorun( self._command, self._config.password )
 		errors = []
 		info = []
-		for line in lines:
-			if line != "" and not line.startswith( "COMMAND " ):
-				parsed = self._lineparser.findall( line )[0]
+		for l in lines:
+			if l != "" and not l.startswith( "COMMAND " ):
+				parsed = self._lineparser.findall( l )[0]
 				if len( parsed ) == 0:
-					errors.append( "FAULT: {}".format( line ) )
+					errors.append( "FAULT: {}".format( l ) )
 				else:
 					details = parsed[-1]
 					src, dest, status = ( "", "", "" )
@@ -77,18 +77,18 @@ class SenseNetwork():
 					dest = dest.split( ":" )
 					status = status.replace( ")", "" ).replace( "(", "" )
 					info.append( [ det for det in parsed[:-1] ] + [src] + [dest] + [status ] )
-		self._info = info
-		self._errors = errors
+		self._lsof_info = info
+		self._lsof_errors = errors
 		return errors, info
 
 if( __name__ == '__main__' ):
 	senseNetwork = SenseNetwork( config )
 	#errors, info = senseNetwork.parse_lsof_information()
-	errors = senseNetwork.geterrors()
-	info = senseNetwork.getinfo()
+	zerrors = senseNetwork.geterrors()
+	zinfo = senseNetwork.getinfo()
 
 	dets = {}
-	for line in info:
+	for line in zinfo:
 		#print line
 		if line[0] not in dets:
 			dets[line[0]] = [ line[1:] ]
@@ -99,9 +99,9 @@ if( __name__ == '__main__' ):
 		print ""
 	#print json.dumps( info, indent=2 )
 
-	if( len( errors ) > 0 ):
+	if( len( zerrors ) > 0 ):
 		print "#" * 50
 		print "      Errors"
 		print "#" * 50
-		print "\n".join( errors )
+		print "\n".join( zerrors )
 
